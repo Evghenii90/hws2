f25ace10287b06557e4c8d27488ca47d952edfde
Evghenii90 <litvinenkoevgeniy3359@gmail.com>
2025-06-04 14:43:51 +0300

    commit hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,16 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            return [...state].sort(
+                (a: any, b: any) => action.payload === 'up'
+                    ? a.name.localeCompare(b.name)
+                    : b.name.localeCompare(a.name))
 
-            return state // need to fix
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter((user: any) => user.age >= action.payload)
+            // need to fix
         }
         default:
             return state
